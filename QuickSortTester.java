fe3667a437c96feb25a138e9b226b7214fe44c13
DimitriyL <dleksanov@stuy.edu>
2017-03-09 20:13:27 -0500

    added new sequences

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -9,7 +9,11 @@ n = 50 Time: 68824.0 ns
 n = 75 Time: 156062.0 ns
 n = 1000 Time : 2388320.0 ns
 n = 5000 Time: 7858635.0 ns
-n  = 10,000 Time: 1.5483932E7 ns
+n = 10,000 Time: 1.5483932E7 ns
+n = 10, ordered:
+n = 10, opposite order:
+n = 10, highest at the ends:
+n = 10, highest at the center:
 
 We calculate the runtime it takes for a QuickSort to run on different arrays
 
@@ -81,21 +85,35 @@ public class QuickSortTester{
 	  data can be gathered from terminal prints
 	 */
 
-	/*
-	int[] arr100000 = new int[100000];
-	start = System.nanoTime();
-	QuickSort.qsort(arr100000);
-	stop = System.nanoTime();
-	System.out.print("Time: ");
-	System.out.println(stop - start);
+        int[] best10 = {6, 13, 17, 22, 33, 34, 54, 55, 75, 83};
+        start = System.nanoTime();
+        QuickSort.qsort(best10);
+        stop = System.nanoTime();
+        System.out.print("Time: ");
+        System.out.println(stop - start);
+	    
+        int[] backwards10 = {83, 75, 55, 54, 34, 33, 22, 17, 13, 6};
+        start = System.nanoTime();
+        QuickSort.qsort(backwards10);
+        stop = System.nanoTime();
+        System.out.print("Time: ");
+        System.out.println(stop - start);
 
-	int[] arr1000000 = new int[1000000];
-	start = System.nanoTime();
-	QuickSort.qsort(arr1000000);
-	stop = System.nanoTime();
-	System.out.print("Time: ");
-	System.out.println(stop - start);
-	*/
+        //higher values to the outside                                          
+        int[] outside = {83, 55, 34, 22, 13, 6, 17, 33, 54, 75};
+        start = System.nanoTime();
+        QuickSort.qsort(outside);
+        stop = System.nanoTime();
+        System.out.print("Time: ");
+        System.out.println(stop - start);
+
+        //higher values to the interior                                         
+        int[] inside = {6, 17, 33, 54, 75, 83, 55, 34, 22, 13};
+        start = System.nanoTime();
+        QuickSort.qsort(inside);
+        stop = System.nanoTime();
+        System.out.print("Time: ");
+        System.out.println(stop - start);
 
     }
 
